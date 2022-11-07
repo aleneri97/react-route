73f72ed8676ea66503d633c48f10a25b8aa8364d
Alejandro Neri <ale_neri@yahoo.com>
2022-11-07 17:44:47 -0600

    Change blog entries to outlets

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Outlet} from 'react-router-dom';
 import {blogdata} from './blogdata';
 
 export const BlogPage = () => {
 	return (
 		<>
 			<h1>BlogPage</h1>
+			<Outlet></Outlet>
 			<ul>
 				{blogdata.map((post) => (
 					<BlogLink key={post.slug} post={post} />

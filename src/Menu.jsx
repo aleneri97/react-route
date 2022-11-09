e4a2eca06f2085889f260f1bc4c28d0f7fc4adbc
Alejandro Neri <ale_neri@yahoo.com>
2022-11-09 17:00:19 -0600

    Modify menu to show links based on session

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,46 +1,56 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
+import {useAuth} from './auth';
 
 export const Menu = () => {
-  const linkActiveStyle = isActive => ({ color: isActive ? 'red' : 'blue' });
+	const linkActiveStyle = (isActive) => ({color: isActive ? 'red' : 'blue'});
+	const auth = useAuth();
 
-  const routes = [
-    {
-      path: '/',
-      text: 'Home'
-    },
-    {
-      path: '/blog',
-      text: 'Blog'
-    },
-    {
-      path: '/profile',
-      text: 'Profile'
-    },
-    {
-      path: '/login',
-      text: 'Login'
-    },
-    {
-      path: '/logout',
-      text: 'Logout'
-    },
-  ];
-  
+	const routes = [
+		{
+			path: '/',
+			text: 'Home',
+			permission: 'all',
+		},
+		{
+			path: '/blog',
+			text: 'Blog',
+			permission: 'all',
+		},
+		{
+			path: '/profile',
+			text: 'Profile',
+			permission: 'private',
+		},
+		{
+			path: '/login',
+			text: 'Login',
+			permission: 'public',
+		},
+		{
+			path: '/logout',
+			text: 'Logout',
+			permission: 'private',
+		},
+	];
+
 	return (
 		<nav>
 			<ul>
-				{routes.map(route => (
+				{routes.map((route) => {
+          if(auth.user && route.permission === 'public') return null;
+          if(!auth.user && route.permission === 'private') return null;
+          return (
 					<li key={route.path}>
 						<NavLink
 							style={({isActive}) => linkActiveStyle(isActive)}
 							to={route.path}
-              end
+							end
 						>
 							{route.text}
 						</NavLink>
-					</li>
-				))}
+					</li>);
+				})}
 			</ul>
 		</nav>
 	);
